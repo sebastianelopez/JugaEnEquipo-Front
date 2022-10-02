3fc25833b168dd4ad91b7959ada99372cb7debc0
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2022-10-01 23:58:25 -0300

    i18n config added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  i18n: {
+    locales: ['en', 'es', 'pt'],
+    defaultLocale: 'en'
+  }
 }
 
 module.exports = nextConfig
