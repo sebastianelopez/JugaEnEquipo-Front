937e5b57b253b863b83e10c178b3977a04c08ab0
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2022-10-02 00:08:58 -0300

    first structure of the project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import "/node_modules/flag-icons/css/flag-icons.min.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from "next/app";
+import { UiProvider } from "../context";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { lightTheme } from "../themes";
+import { NextIntlProvider } from "next-intl";
+
+import type { NextPageContext  } from 'next' 
+
+type CustomAppProps = AppProps & {
+  pageProps: NextPageContext & {messages?: {} } 
 }
 
-export default MyApp
+function MyApp({ Component, pageProps}: CustomAppProps) {
+  return (
+    <UiProvider>
+      <ThemeProvider theme={lightTheme}>
+        <NextIntlProvider messages={pageProps.messages}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </NextIntlProvider>
+      </ThemeProvider>
+    </UiProvider>
+  );
+}
+
+export default MyApp;
