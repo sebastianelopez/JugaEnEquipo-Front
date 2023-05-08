41511deeb6298869dfd5eee3546d92dd0eb87128
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2023-05-07 22:44:25 -0300

    UserOrGroupItem component created

---
+++ components/organisms/UserOrGroupList/UserOrGroupItem.tsx
@@ -0,0 +1,24 @@
+import { Grid } from "@mui/material";
+import { useTranslations } from "next-intl";
+import { FC } from "react";
+import { User, Group } from "../../../interfaces";
+
+interface Props {
+    usersOrGroups: User[] | Group[] ;
+}
+
+export const UserOrGroupItem: FC<Props> = ({ usersOrGroups }) => {
+    const t = useTranslations("Publication");
+  return (
+    <Grid container spacing={4}>
+        {
+            usersOrGroups.map( usersOrGroup => (
+                <ProductCard 
+                    key={ product.slug }
+                    product={ product }
+                />
+            ))
+        }
+    </Grid>
+  )
+}
