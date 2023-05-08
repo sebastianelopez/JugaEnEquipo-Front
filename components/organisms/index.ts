9b241ebc1f3e5f6a4dfab74e5bcd1ea6ff32a295
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2023-05-08 00:03:29 -0300

    some exports added to index file

--- components/organisms/index.ts
+++ components/organisms/index.ts
@@ -1,5 +1,4 @@
 
-
 //forms
 export { LoginForm } from './form/LoginForm';
 export { RegisterForm } from './form/RegisterForm';
@@ -9,6 +8,19 @@ export { RegisterForm } from './form/RegisterForm';
 export { AuthNavbar } from './navbar/AuthNavbar';
 export { MainNavbar } from './navbar/MainNavbar';
 export { HomeNavbar } from './navbar/HomeNavbar';
+
+//Menu
+export { SideMenu } from './menu/SideMenu';
+
+//Headers
+export { HomeHeader } from './headers/HomeHeader';
+
+//Cards
+export { IdentityCard } from './cards/IdentityCard';
+export { PublicationCard } from './cards/PublicationCard';
+
+//Others
+export { UserOrGroupList } from './UserOrGroupList/UserOrGroupList';
 
 
 
