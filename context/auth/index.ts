38282470af18a6b188aa43efa6bcf53d14f5d8a2
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2025-05-11 19:24:34 -0300

    remove unnecessary auth context

--- context/auth/index.ts
+++
@@ -1,4 +0,0 @@
-export type { AuthState } from './AuthProvider';
-export { authReducer } from "./authReducer";
-export { AuthContext } from './AuthContext';
-export { AuthProvider } from "./AuthProvider";
