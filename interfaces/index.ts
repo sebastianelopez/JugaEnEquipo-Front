70104ba02cd2a05c60309b46c986b02435014060
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2025-05-11 19:37:10 -0300

    update index base on new interfaces

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,5 +1,7 @@
 export type { Group } from './group';
 export type { User } from './user';
 export type { Game } from './game';
+export type { Resource } from './resource';
+export type { Post } from './post';
 
 
