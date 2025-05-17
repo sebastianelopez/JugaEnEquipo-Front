f0730159609b479dcb03e7c88fa92b8744e4ffe5
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2025-05-17 20:08:14 -0300

    remove select country from navbar

--- components/organisms/navbar/HomeNavbar.tsx
+++ components/organisms/navbar/HomeNavbar.tsx
@@ -1,38 +1,27 @@
-import { SearchOutlined, ShoppingCartOutlined } from "@mui/icons-material";
+import { SearchOutlined } from "@mui/icons-material";
 import {
   AppBar,
   Box,
   Button,
   IconButton,
   Link,
-  MenuItem,
-  Select,
   Toolbar,
   Typography,
 } from "@mui/material";
 import Image from "next/image";
 import NextLink from "next/link";
 import { useRouter } from "next/router";
-import { useContext, useEffect, useState } from "react";
-import { UiContext } from "../../../context";
+import { useEffect } from "react";
 import { gsap } from "gsap";
 import { useTranslations } from "next-intl";
+import { SelectCountry } from "../../molecules/SelectCountry";
 
 import logo from "./../../../assets/logo.png";
 
 export const HomeNavbar = () => {
-  const { asPath, locale, push, query, pathname } = useRouter();
-
-  const { toggleSideMenu } = useContext(UiContext);
+  const { asPath } = useRouter();
 
   const t = useTranslations("Navbar");
-
-  const [selectValue, setSelectValue] = useState(locale ? locale : "en");
-
-  const onSelectChange = (newLocale: string) => {
-    setSelectValue(newLocale);
-    push({ pathname, query }, asPath, { locale: newLocale });
-  };
 
   const logotitle = ".logotitle";
 
@@ -55,7 +44,12 @@ export const HomeNavbar = () => {
               alignItems="center"
               component={"div"}
             >
-              <Image src={logo} height={40} width={70} alt="Juga en Equipo logo" />
+              <Image
+                src={logo}
+                height={40}
+                width={70}
+                alt="Juga en Equipo logo"
+              />
               <Typography className="logotitle">Juga en equipo</Typography>
             </Box>
           </Link>
@@ -71,9 +65,7 @@ export const HomeNavbar = () => {
         >
           <NextLink href={"/"} passHref>
             <Link component="span">
-              <Button color={asPath === "/" ? "primary" : "info"}>
-                Home
-              </Button>
+              <Button color={asPath === "/" ? "primary" : "info"}>Home</Button>
             </Link>
           </NextLink>
           <NextLink href={"/category/women"} passHref>
@@ -92,21 +84,7 @@ export const HomeNavbar = () => {
           </NextLink>
         </Box>
         <Box flex={1} component="div" />
-        <Select
-          variant="outlined"
-          value={selectValue}
-          onChange={(e) => onSelectChange(e.target.value)}
-        >
-          <MenuItem value={"es"}>
-            <span className="fi fi-ar" />
-          </MenuItem>
-          <MenuItem value={"en"}>
-            <span className="fi fi-us" />
-          </MenuItem>
-          <MenuItem value={"pt"}>
-            <span className="fi fi-br" />
-          </MenuItem>
-        </Select>
+        <SelectCountry />
 
         <IconButton>
           <SearchOutlined />
