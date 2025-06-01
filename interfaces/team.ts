3059ed7a471ef7df3837a52471059d30b00aafc9
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2025-06-01 20:49:54 -0300

    replace group for team interface

--- interfaces/team.ts
+++ interfaces/team.ts
@@ -1,7 +1,9 @@
 import { User } from "./user";
 
-export interface Group{
+export interface Team{
+    _id: string;
     name: string;
     users: User[];
     achievements?: string[];
+    profileImage?: string;
 }
