8d4cb0b047518d90a3a323c214402f36c9753fcc
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2025-06-01 20:46:34 -0300

    basic ui and logic for serach page

---
+++ pages/search/mock.ts
@@ -0,0 +1,99 @@
+import { Team, User } from "../../interfaces";
+
+export const mockSearchResults = {
+  users: [
+    {
+      _id: "user1",
+      username: "gamer123",
+      profileImage: "/images/avatars/avatar1.jpg",
+      games: [
+        { name: "League of Legends", elo: 1500 },
+        { name: "Valorant", elo: 2200 },
+      ],
+    },
+    {
+      _id: "user2",
+      username: "proGamer",
+      profileImage: "/images/avatars/avatar2.jpg",
+      games: [
+        { name: "Counter-Strike", elo: 2300 },
+        { name: "Dota 2", elo: 1800 },
+      ],
+    },
+    {
+      _id: "user3",
+      username: "casualPlayer",
+      profileImage: "/images/avatars/avatar3.jpg",
+      games: [
+        { name: "Fortnite", elo: 800 },
+        { name: "Apex Legends", elo: 950 },
+      ],
+    },
+    {
+      _id: "user4",
+      username: "midRanker",
+      profileImage: "/images/avatars/avatar4.jpg",
+      games: [
+        { name: "League of Legends", elo: 1200 },
+        { name: "Overwatch", elo: 1600 },
+      ],
+    },
+    {
+      _id: "user5",
+      username: "topPlayer",
+      profileImage: "/images/avatars/avatar5.jpg",
+      games: [
+        { name: "Valorant", elo: 2500 },
+        { name: "Counter-Strike", elo: 2100 },
+      ],
+    },
+    {
+      _id: "user6",
+      username: "newbieGamer",
+      profileImage: "/images/avatars/avatar6.jpg",
+      games: [
+        { name: "Fortnite", elo: 600 },
+        { name: "League of Legends", elo: 900 },
+      ],
+    },
+  ] as User[],
+
+  teams: [
+    {
+      _id: "team1",
+      name: "Pro Gamers",
+      profileImage: "/images/teams/logo1.jpg",
+      users: [],
+    },
+    {
+      _id: "team2",
+      name: "Casual Squad",
+      profileImage: "/images/teams/logo2.jpg",
+      users: [],
+    },
+    {
+      _id: "team3",
+      name: "Competitive Crew",
+      profileImage: "/images/teams/logo3.jpg",
+      users: [],
+    },
+    {
+      _id: "team4",
+      name: "Weekend Warriors",
+      profileImage: "/images/teams/logo4.jpg",
+      users: [],
+    },
+    {
+      _id: "team5",
+      name: "Elite Squad",
+      profileImage: "/images/teams/logo5.jpg",
+      users: [],
+    },
+    {
+      _id: "team6",
+      name: "Newbie Friendly",
+      profileImage: "/images/teams/logo6.jpg",
+      users: [],
+    },
+  ] as Team[],
+};
