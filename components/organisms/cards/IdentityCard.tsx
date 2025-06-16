fc8daa675158d1164771ed3336e626c246ce19d0
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2025-06-15 22:55:25 -0300

    add margin bottom to avatar and format code

--- components/organisms/cards/IdentityCard.tsx
+++ components/organisms/cards/IdentityCard.tsx
@@ -4,17 +4,25 @@ import { FC } from "react";
 import { User } from "../../../interfaces";
 
 interface Props {
-    user: User
+  user: User;
 }
 
-export const IdentityCard: FC<Props> = ({user}) => {
+export const IdentityCard: FC<Props> = ({ user }) => {
   const t = useTranslations("Publication");
   return (
-    <Paper sx={{ p: 2, textAlign: "center", width:"100%", maxWidth: '250px', mb:3}}>
+    <Paper
+      sx={{
+        p: 2,
+        textAlign: "center",
+        width: "100%",
+        maxWidth: "250px",
+        mb: 3,
+      }}
+    >
       <Avatar
         alt="Profile Picture"
         src={user.profileImage}
-        sx={{ width: 200, height: 200, m: "auto" }}
+        sx={{ width: 200, height: 200, m: "auto", marginBottom: 1 }}
       />
       <Typography variant="h4" fontWeight="bold">
         {user.username}
