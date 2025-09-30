bb7653a1967256bc1a65335a45b1b8915d85be25
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2025-09-29 21:53:15 -0300

    format code

--- components/organisms/index.ts
+++ components/organisms/index.ts
@@ -1,40 +1,35 @@
-
 //forms
-export { LoginForm } from './form/LoginForm';
-export { RegisterForm } from './form/RegisterForm';
-
+export { LoginForm } from "./form/LoginForm";
+export { RegisterForm } from "./form/RegisterForm";
 
 //navs
-export { AuthNavbar } from './navbar/AuthNavbar';
-export { MainNavbar } from './navbar/MainNavbar';
-export { HomeNavbar } from './navbar/HomeNavbar';
+export { AuthNavbar } from "./navbar/AuthNavbar";
+export { MainNavbar } from "./navbar/MainNavbar";
+export { HomeNavbar } from "./navbar/HomeNavbar";
 
 //Menu
-export { SideMenu } from './menu/SideMenu';
+export { SideMenu } from "./menu/SideMenu";
 
 //Headers
-export { HomeHeader } from './headers/HomeHeader';
+export { HomeHeader } from "./headers/HomeHeader";
 
 //Footer
-export { Footer } from './footer/Footer';
+export { Footer } from "./footer/Footer";
 
 //Cards
-export { IdentityCard } from './cards/IdentityCard';
-export { PublicationCard } from './cards/PublicationCard/PublicationCard';
-export { HashtagsCard } from './cards/HashtagsCard';
-export { PublicateCard } from './cards/PublicateCard';
-export { ProfileCard } from './cards/ProfileCard';
+export { IdentityCard } from "./cards/IdentityCard";
+export { PublicationCard } from "./cards/PublicationCard/PublicationCard";
+export { HashtagsCard } from "./cards/HashtagsCard";
+export { PublicateCard } from "./cards/PublicateCard";
+export { ProfileCard } from "./cards/ProfileCard";
+export { UpcomingEventsCard } from "./cards/UpcomingEventsCard";
 
 //Tables
-export { TournamentTable } from './tables/TournamentTable';
+export { TournamentTable } from "./tables/TournamentTable";
 
 //Modals
-export { CreatePublicationModal } from './modals/CreatePublicationModal';
+export { CreatePublicationModal } from "./modals/CreatePublicationModal";
 
 //Others
-export { HomeNews } from './HomeNews/HomeNews';
-export { SliderText } from './sliderText/SliderText';
-
-
-
-
+export { HomeNews } from "./HomeNews/HomeNews";
+export { SliderText } from "./sliderText/SliderText";
