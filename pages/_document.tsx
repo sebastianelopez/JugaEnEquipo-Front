1ef3db288003b40fbdaa720c766ae2840fa82ea3
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2025-11-30 17:48:43 -0300

    add fav icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,7 +20,39 @@ class MyDocument extends Document {
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />          
+          />
+          {/* Favicons */}
+          <link rel="icon" type="image/x-icon" href="/favicon/favicon.ico" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png" />
+          
+          {/* Apple Touch Icons */}
+          <link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png" />
+          <link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png" />
+          <link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-icon-72x72.png" />
+          <link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-icon-76x76.png" />
+          <link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-icon-114x114.png" />
+          <link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-icon-120x120.png" />
+          <link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-icon-144x144.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-icon-152x152.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon-180x180.png" />
+          
+          {/* Android Icons */}
+          <link rel="icon" type="image/png" sizes="36x36" href="/favicon/android-icon-36x36.png" />
+          <link rel="icon" type="image/png" sizes="48x48" href="/favicon/android-icon-48x48.png" />
+          <link rel="icon" type="image/png" sizes="72x72" href="/favicon/android-icon-72x72.png" />
+          <link rel="icon" type="image/png" sizes="96x96" href="/favicon/android-icon-96x96.png" />
+          <link rel="icon" type="image/png" sizes="144x144" href="/favicon/android-icon-144x144.png" />
+          <link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-icon-192x192.png" />
+          
+          {/* Microsoft Tiles */}
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="msapplication-TileImage" content="/favicon/ms-icon-144x144.png" />
+          <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+          
+          {/* Web App Manifest */}
+          <link rel="manifest" href="/favicon/manifest.json" />
         </Head>
         <body>
           <Main />
