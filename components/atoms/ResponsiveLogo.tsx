4fc2c8cc87cb78389d60b442f2d1dfc5bf5eb9f9
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2025-12-09 20:25:22 -0300

    use violet logo version for landing navbar

--- components/atoms/ResponsiveLogo.tsx
+++ components/atoms/ResponsiveLogo.tsx
@@ -2,8 +2,10 @@ import { Box, BoxProps } from "@mui/material";
 import Image from "next/image";
 import logo from "../../assets/juga-en-equipo_ISO-2-1.png";
 import logoRightText from "../../assets/juga-en-equipo_blanco-1.png";
+import logoRightTextViolet from "../../assets/juga-en-equipo_violeta-1.png";
+import logoViolet from "../../assets/juga-en-equipo_ISO-1-1.png";
 
-interface ResponsiveLogoProps extends Omit<BoxProps, 'children'> {
+interface ResponsiveLogoProps extends Omit<BoxProps, "children"> {
   /**
    * Size variant for the logos
    * @default "medium"
@@ -14,27 +16,33 @@ interface ResponsiveLogoProps extends Omit<BoxProps, 'children'> {
    * @default "Juga en Equipo logo"
    */
   altText?: string;
+  /**
+   * Whether to use the violet logo
+   * @default false
+   */
+  violet?: boolean;
 }
 
 const sizeConfig = {
   small: {
     mobile: { height: 35, width: 39 }, // ISO-2 aspect ratio: ~1.13:1
-    desktop: { height: 35, width: 79 } // blanco aspect ratio: ~2.26:1
+    desktop: { height: 35, width: 79 }, // blanco aspect ratio: ~2.26:1
   },
   medium: {
     mobile: { height: 45, width: 51 }, // ISO-2 aspect ratio: ~1.13:1
-    desktop: { height: 45, width: 102 } // blanco aspect ratio: ~2.26:1
+    desktop: { height: 45, width: 102 }, // blanco aspect ratio: ~2.26:1
   },
   large: {
     mobile: { height: 55, width: 62 }, // ISO-2 aspect ratio: ~1.13:1
-    desktop: { height: 55, width: 124 } // blanco aspect ratio: ~2.26:1
-  }
+    desktop: { height: 55, width: 124 }, // blanco aspect ratio: ~2.26:1
+  },
 };
 
-export const ResponsiveLogo = ({ 
-  size = "medium", 
+export const ResponsiveLogo = ({
+  size = "medium",
   altText = "Juga en Equipo logo",
-  ...boxProps 
+  violet = false,
+  ...boxProps
 }: ResponsiveLogoProps) => {
   const { mobile, desktop } = sizeConfig[size];
 
@@ -53,7 +61,7 @@ export const ResponsiveLogo = ({
         }}
       >
         <Image
-          src={logo}
+          src={violet ? logoViolet : logo}
           height={mobile.height}
           width={mobile.width}
           alt={altText}
@@ -67,7 +75,7 @@ export const ResponsiveLogo = ({
         }}
       >
         <Image
-          src={logoRightText}
+          src={violet ? logoRightTextViolet : logoRightText}
           height={desktop.height}
           width={desktop.width}
           alt={altText}
